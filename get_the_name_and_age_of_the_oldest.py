33c2d9f991dfc9d7da644372aace9e5d71994111
unknown <johnmichaelalarcon2006@gmail.com>
2024-10-21 19:11:02 +0700

    input loop

---
+++ get_the_name_and_age_of_the_oldest.py
@@ -0,0 +1,8 @@
+individuals = {} # Initialize individuals as an empty dictionary
+
+#  Loop: asks the user to input
+while True:
+    name = input("Enter your name: ")
+    age = int(input("Enter your age: "))
+
+
