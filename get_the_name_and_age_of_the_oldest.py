6c2e47d2897e0ce154676a0cbb114f062367876e
unknown <johnmichaelalarcon2006@gmail.com>
2024-10-21 19:45:33 +0700

    name and age validation comments

--- get_the_name_and_age_of_the_oldest.py
+++ get_the_name_and_age_of_the_oldest.py
@@ -1,21 +1,21 @@
-def is_valid_name(name):
-    return name.isalpha() and len(name) 
+def is_valid_name(name): # This function checks if a given name is valid
+    return name.isalpha() and len(name)  # Checks if the name contains only alphabetic characters and not empty
 
 def main():
     individuals = {} # Initialize individuals as an empty dictionary
 
-    #  Loop: asks the user to input
+    #  Loop: Asks the user to input
     while True:
         name = input("Enter your name: ")
-        if not is_valid_name(name):
+        if not is_valid_name(name): # Checks if the name is valid using is_valid_name()
             print("Error! Invalid name. Please enter a valid name (letters only).")
             continue
 
         age = int(input("Enter your age: "))
-        try: 
-            if age < 0 and age > 100:
+        try: # Used to catch potential errors
+            if age < 0 or age > 100:  
                 raise ValueError("Age must be between 0 and 100")
-        except ValueError as e:
+        except ValueError as e: # ValueError would be raised if the age is invalid
             print(f"Error! {e} Please enter a valid age")
-            continue
+            continue 
 
